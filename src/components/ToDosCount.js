7030c380fa647c8ab4213c3d1e5c6cd1257a26fb
Kurt Petrek <kurtpetrek@gmail.com>
2017-08-30 20:52:04 -0400

    Updates

--- src/components/ToDosCount.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ToDosCount(props) {
-  return (
-    <div className="well well-sm">
-      <h4>Total Todos: {props.toDosCount}</h4>
-    </div>
-  )
-}
